bb2386794ed0bcd051c0f6df81c9d414425811ff
nitadori <keigo@riken.jp>
2018-11-27 19:19:03 +0900

    division

--- ad2.hpp
+++ ad2.hpp
@@ -25,4 +25,14 @@ struct AD2{
 
 		return {dd0, dd1};
 	}
+
+	AD2 operator/(const AD2 &rhs) const {
+		T ginv = T(1) / rhs.d0;
+		T ginv2 = ginv * ginv;
+
+		T dd0 = d0 * ginv;
+		T dd1 = (rhs.d0 * d1 - d0 * rhs.d1) * ginv2;
+
+		return {dd0, dd1};
+	}
 };
